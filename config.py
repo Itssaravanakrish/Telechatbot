1dd478265df5aac785f7d15de191721f2a8ae9f1
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2024-01-29 18:21:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+TOKEN = os.environ.get("TOKEN", "6318256885:AAGgylgIY6zZXlegJfFdJO5QJOogSFhQAxs") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "saravanakrish") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "TamilChatGroup") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "TamilBota") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
 DB_URI = os.environ.get("DATABASE_URL", "")
