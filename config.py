5c465ba427a9c14bde9f0c90dbad2132a5ce82be
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2024-01-29 18:31:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "6318256885:AAGgylgIY6zZXlegJfFdJO5QJOogSFhQAxs") # Dapatkan di @botfather
+TOKEN = os.environ.get("TOKEN", "6318256885:AAFvBL_LqoPm0rXavgsegREqnwRxNCqTAvM") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "saravanakrish") # Isi dengan username kalian tanpa tanda @
 GROUP = os.environ.get("GROUP", "TamilChatGroup") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
 CHANNEL = os.environ.get("CHANNEL", "TamilBota") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
